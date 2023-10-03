5865c8d8808bedff4c82829331b4f567cc8fc074
sboulal <rawanasmaa51@gmail.com>
2023-10-03 13:47:26 -0400

    exit status fixed

--- executing/builtins/exit.c
+++ executing/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: saboulal  <saboulal@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 15:47:22 by nkhoudro          #+#    #+#             */
-/*   Updated: 2023/09/28 20:58:31 by saboulal         ###   ########.fr       */
+/*   Updated: 2023/10/03 13:45:13 by saboulal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,7 +90,7 @@ void	exit_program(t_mini *cmd, t_exec *exp)
 			ft_putstr_fd("minishell: exit: ", 2);
 			ft_putstr_fd(cmd->arg[0], 2);
 			ft_putstr_fd(": numeric argument required\n", 2);
-			g_var.status = 255;
+			g_var.status = 2;
 		}
 	// 	// else
 	// 	// {
@@ -121,7 +121,7 @@ void	exit_program(t_mini *cmd, t_exec *exp)
 			if (!ft_isdigit(cmd->arg[0][i]) && cmd->arg[0][i] != '+' && cmd->arg[0][i] != '-')
 			{
 				ft_putstr_fd("exit\n", 2);
-				g_var.status = 255;
+				g_var.status = 2;
 				ft_putstr_fd("minishell: exit: ", 2);
 				ft_putstr_fd(cmd->arg[0], 2);
 				ft_putstr_fd(": numeric argument required\n", 2);
